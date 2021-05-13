ea8d4722259c12f40f1d355997b8e5201919a501
alekssandra8 <savic838@gmail.com>
2021-05-13 19:21:19 +0200

    staticka biblioteka

--- Usart/usart.c
+++ Usart/usart.c
@@ -3,14 +3,12 @@
 #include <avr/pgmspace.h>
 #include "usart.h"
 
-#define UART_BUFFER_SIZE 64
-
 int8_t Rx_Buffer[UART_BUFFER_SIZE];		// Prijemni FIFO bafer
 volatile uint8_t Rx_Buffer_Size = 0;	// Broj karaktera u prijemnom baferu
-volatile uint8_t Rx_Buffer_First = 0;
+volatile uint8_t Rx_Buffer_First = 0;   // char = uint8_t !
 volatile uint8_t Rx_Buffer_Last = 0;
 
-ISR(USART_RX_vect)
+ISR(USART_RX_vect)   // koristimo rx prekid ovde!
 {
 	Rx_Buffer[Rx_Buffer_Last++] = UDR0;		// Ucitavanje primljenog karaktera
 	Rx_Buffer_Last &= UART_BUFFER_SIZE - 1;	// Povratak na pocetak u slucaju prekoracenja
@@ -25,14 +23,14 @@ void usartInit(uint32_t baud)
 	 * U2Xn = 0: Onemogucena dvostruka brzina
 	 * MPCMn = 0: Onemogucen multiprocesorski rezim
 	 */
-	UCSR0A = 0x20;	
+	UCSR0A = 0x20;	// 0010 0000 -- bit5
 
 	/*
 	 * RXCIEn = 1: Dozvola prekida izavanog okoncanjem prijema
 	 * RXENn = 1: Dozvola prijema
 	 * TXENn = 1: Dozvola slanja
 	 */
-	UCSR0B = 0x98;
+	UCSR0B = 0x98; // 1001 1000
 
 	/*
 	 * UMSELn[1:0] = 00: Asinroni rezim
@@ -40,7 +38,7 @@ void usartInit(uint32_t baud)
 	 * USBSn = 0: Koristi se jedan stop bit
 	 * UCSzn[2:0] = 011: 8-bitni prenos
 	 */
-	UCSR0C = 0x06;
+	UCSR0C = 0x06; // 0000 0110
 
 	uint16_t UBRRn = F_CPU / (16 * baud) - 1;
 	UBRR0H = UBRRn >> 8;
@@ -54,13 +52,13 @@ uint8_t usartAvailable()
 	return Rx_Buffer_Size;		// Ocitavanje broja karaktera u prijemnom baferu
 }
 
-void usartPutChar(int8_t c)
+void usartPutChar(int8_t c) // salje 1 karakter
 {
 	while(!(UCSR0A & 0x20));	// Ceka da se setuje UDREn (indikacija da je predajni bafer prazan)
 	UDR0 = c;					// Upis karaktera u predajni bafer
 }
 
-void usartPutString(int8_t *s)
+void usartPutString(int8_t *s) // salje niz karaktera - string
 {
 	while(*s != 0)				// Petlja se izvrsava do nailaska na nul-terminator
 	{
@@ -91,7 +89,7 @@ int8_t usartGetChar()
 	Rx_Buffer_Size--;							// Dekrement brojaca karaktera u prijemnom baferu
 
 	return c;
-}
+} // 88. red - ANDujemo sa 64 i jos -1 i vrati ga na 0!
 
 uint8_t usartGetString(int8_t *s)
 {
@@ -102,4 +100,43 @@ uint8_t usartGetString(int8_t *s)
 
 	s[len] = 0;						// Terminacija stringa
 	return len;						// Vraca broj ocitanih karaktera
+}
+
+int8_t usartPeek()
+{
+	int8_t c;
+
+	if (!Rx_Buffer_Size)
+		return -1;
+	c = Rx_Buffer[Rx_Buffer_First];
+
+	return c;
+}
+
+int16_t usartParseInt()
+{
+	int8_t c;
+	uint16_t res = 0;
+	int8_t sign = 1;
+
+	// odstranimo sve karaktere koji nisu minus ili cifra
+	while((usartPeek() < '0' && usartPeek() != '-') || usartPeek() > '9')
+	{
+		c = usartGetChar();
+	}
+
+	if(usartPeek() == '-')
+	{
+		c = usartGetChar();
+		sign = -1;
+
+	}
+	// izdvajanje cifara i dobijanje celog broja
+	while((usartPeek() >= '0' && usartPeek() <= '9'))
+	{
+		c = usartGetChar();
+		res = res * 10 + (c - '0');
+	}
+
+	return res * sign;
 }
